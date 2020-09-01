299f9c100bda396f6d9867721cbd3a196552185b
ridomin <ridomin@live.com>
2020-08-31 17:16:55 -0700

    add parser experiments

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-/** @type {Array<modelInfo>} models */
-let models = []
-
-/**
- * @param {string} id - element id
- * @returns {HTMLElement}
- */
-const gbid = (id) => {
-  const el = document.getElementById(id)
-  if (el === null) {
-    throw new Error('element not found: ' + id)
-  }
-  return el
-}
-
-/**
- * @returns {Promise<Array<modelInfo>>}
- */
-const loadModels = () => {
-  return new Promise((resolve, reject) => {
-    window.fetch('model-index.json')
-      .then(r => r.json())
-      .then(m => resolve(m.models))
-      .catch(e => reject(e))
-  })
-}
-
-/**
- * @param {string} template
- * @param {Array<modelInfo>} models
- * @param {string} target
- */
-const bindTemplate = (template, models, target) => {
-  gbid(target).innerHTML = Mustache.render(gbid(template).innerHTML, models)
-}
-
-const init = async () => {
-  models = await loadModels()
-  bindTemplate('models-list-template', models, 'rendered')
-}
-
 (async () => {
+  /** @type {Array<modelInfo>} models */
+  let models = []
+
+  /**
+   * @param {string} id - element id
+   * @returns {HTMLElement}
+   */
+  const gbid = (id) => {
+    const el = document.getElementById(id)
+    if (el === null) {
+      throw new Error('element not found: ' + id)
+    }
+    return el
+  }
+
+  /**
+   * @returns {Promise<Array<modelInfo>>}
+   */
+  const loadModels = () => {
+    return new Promise((resolve, reject) => {
+      window.fetch('model-index.json')
+        .then(r => r.json())
+        .then(m => resolve(m.models))
+        .catch(e => reject(e))
+    })
+  }
+
+  /**
+   * @param {string} template
+   * @param {Array<modelInfo>} models
+   * @param {string} target
+   */
+  const bindTemplate = (template, models, target) => {
+    gbid(target).innerHTML = Mustache.render(gbid(template).innerHTML, models)
+  }
+
+  const init = async () => {
+    models = await loadModels()
+    bindTemplate('models-list-template', models, 'rendered')
+  }
   await init()
 })()
---
+++ repo-tool/Program.cs
@@ -0,0 +1,72 @@
+﻿using Azure.DigitalTwins.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace repo_tool
+{
+    class Program
+    {
+        static ModelParser modelParser = new ModelParser();
+        static async Task Main(string[] args)
+        {
+            List<string> modelsContents = new List<string>();
+            if (args.Length<1)
+            {
+                args = new string[] { "." };
+            }
+            string input = args[0];
+            if (!File.Exists(input) && !Directory.Exists(input))
+            {
+                Console.WriteLine($"Error: '{input}' not found.");
+                return;
+            }
+
+            if (File.Exists(input))
+            {
+                modelsContents.Add(File.ReadAllText(input));
+            }
+            if (Directory.Exists(input))
+            {
+                foreach(var file in  Directory.GetFiles(input))
+                {
+                    modelsContents.Add(File.ReadAllText(file));
+                }
+            }
+
+            try
+            {
+                modelParser.DtmiResolver = DtmiResolver;
+                var parsedDtmis = await modelParser.ParseAsync(modelsContents);
+                var interfaces = parsedDtmis.Where(r => r.Value.EntityKind == DTEntityKind.Interface).ToList();
+                foreach (var dt in interfaces)
+                {
+                    Console.WriteLine(dt.Key.AbsoluteUri + " " + dt.Value.DefinedIn);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+
+        static private async Task<IEnumerable<string>> DtmiResolver(IReadOnlyCollection<Dtmi> dtmis)
+        {
+            List<String> jsonLds = new List<string>();
+            foreach (var dtmi in dtmis)
+            {
+                Console.WriteLine("Resolver looking for. " + dtmi);
+                string model = "";//await ModelRepo.GetMissingModel(dtmi.AbsoluteUri);
+                if (!String.IsNullOrWhiteSpace(model))
+                {
+                    jsonLds.Add(model);
+                }
+            }
+            return jsonLds;
+        }
+    }
+}
