e32965cc0bff535066bd391034ce8c78ae5e1975
ridomin <ridomin@live.com>
2020-08-31 23:14:34 -0700

    recurse repo

--- repo-tool/Program.cs
+++ repo-tool/Program.cs
@@ -1,15 +1,20 @@
 ﻿using Azure.DigitalTwins.Parser;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace repo_tool
 {
     class Program
     {
         static ModelParser modelParser = new ModelParser();
+        static List<string> resolverPaths = new List<string>();
+        static string rootInterface = string.Empty;
         static async Task Main(string[] args)
         {
             List<string> modelsContents = new List<string>();
@@ -26,10 +31,14 @@ namespace repo_tool
 
             if (File.Exists(input))
             {
+                var inputFi = new FileInfo(input);
+                rootInterface = inputFi.Name;
+                resolverPaths.Add(inputFi.Directory.FullName);
                 modelsContents.Add(File.ReadAllText(input));
             }
             if (Directory.Exists(input))
             {
+                resolverPaths.Add(input);
                 foreach(var file in  Directory.GetFiles(input))
                 {
                     modelsContents.Add(File.ReadAllText(file));
@@ -57,16 +66,63 @@ namespace repo_tool
         static private async Task<IEnumerable<string>> DtmiResolver(IReadOnlyCollection<Dtmi> dtmis)
         {
             List<String> jsonLds = new List<string>();
+            //other files in local folder as the root interface
+            //foreach (var item in resolverPaths)
+            //{
+            //    foreach (var f in Directory.GetFiles(item))
+            //    {
+            //        if (!f.Contains("model-manifest.json") && !f.Contains(rootInterface))
+            //        { 
+            //            jsonLds.Add(File.ReadAllText(f));
+            //        }
+            //    }
+            //}
+
+
             foreach (var dtmi in dtmis)
             {
-                Console.WriteLine("Resolver looking for. " + dtmi);
-                string model = "";//await ModelRepo.GetMissingModel(dtmi.AbsoluteUri);
-                if (!String.IsNullOrWhiteSpace(model))
-                {
-                    jsonLds.Add(model);
-                }
+                GetModel(dtmi.AbsoluteUri);
             }
-            return jsonLds;
+
+            return await Task.FromResult(allModels.Values.ToList<string>());
         }
+        
+        static IDictionary<string, string> allModels = new Dictionary<string, string>();
+
+        static void GetModel(string dtmi)
+        {
+            Console.WriteLine(". . . downloading " + dtmi);
+
+            string dtmi2Folder(string dtmi)
+            {
+                var allparts = dtmi.ToLower().Split(";")[0].Split(":");
+                var parts = allparts.Skip(1);
+                return string.Join("-", parts);
+            }
+
+            string folder = dtmi2Folder(dtmi);
+            string url = "https://ridomin.github.io/ridomodels/models/";
+
+            var wc = new WebClient();
+            var manifest = wc.DownloadString(new Uri(url +$"{folder}/model-manifest.json"));
+            var manifestItems = JsonConvert.DeserializeObject<IEnumerable<ManifestItem>>(manifest);
+            var root = manifestItems.Where(mi => mi.dtmi == dtmi).FirstOrDefault();
+            if (!allModels.ContainsKey(dtmi))
+            { 
+                allModels.Add(dtmi, wc.DownloadString(url + $"{folder}/{root.path}"));
+            }
+            foreach (var d in root.depends)
+            {
+                GetModel(d);
+            }
+        }
+    }
+
+    class ManifestItem
+    {
+        public string dtmi { get; set; }
+        public string path { get; set; }
+        public string owner { get; set; }
+        public IEnumerable<string> depends { get; set; } = new List<string>();
     }
 }
