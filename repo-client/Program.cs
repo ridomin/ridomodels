58ca3846095ce0c69d04924cda556c798302ad6b
ridomin <ridomin@live.com>
2020-09-01 13:33:28 -0700

    Add repo-client

---
+++ repo-client/Program.cs
@@ -0,0 +1,87 @@
+﻿using Azure.DigitalTwins.Parser;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace repo_client
+{
+    class ManifestItem
+    {
+        public string dtmi { get; set; }
+        public string path { get; set; }
+        public string owner { get; set; }
+        public IEnumerable<string> depends { get; set; } = new List<string>();
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length <1 )
+            {
+                Console.WriteLine("Usage: repo-client <dtmi>");
+                return;
+            }
+            string rootDtmi = args[0];
+            GetModel(rootDtmi);
+            
+            ModelParser parser = new ModelParser();
+            
+            try
+            {
+                //modelParser.DtmiResolver = LocalDtmiResolver;
+                var parsedDtmis = await parser.ParseAsync(allModels.Values);
+                var interfaces = parsedDtmis.Where(r => r.Value.EntityKind == DTEntityKind.Interface).ToList();
+                foreach (var dt in interfaces)
+                {
+                    Console.WriteLine(dt.Key.AbsoluteUri + " " + dt.Value.DefinedIn);
+                    var ifcs = parsedDtmis.Where(r => r.Value.DefinedIn == dt.Key);
+                    foreach (var i in ifcs)
+                    {
+                        Console.WriteLine(i.Value.Id);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        static IDictionary<string, string> allModels = new Dictionary<string, string>();
+
+        static void GetModel(string dtmi)
+        {
+            Console.WriteLine(". . . resolving " + dtmi);
+
+            string dtmi2Folder(string dtmi)
+            {
+                var allparts = dtmi.ToLower().Split(";")[0].Split(":");
+                var parts = allparts.Skip(1);
+                return string.Join("-", parts);
+            }
+
+            string folder = dtmi2Folder(dtmi);
+            string url = "https://ridomin.github.io/ridomodels/models/";
+
+            var wc = new WebClient();
+            var manifest = wc.DownloadString(new Uri(url + $"{folder}/model-manifest.json"));
+            var manifestItems = JsonConvert.DeserializeObject<IEnumerable<ManifestItem>>(manifest);
+            var root = manifestItems.Where(mi => mi.dtmi == dtmi).FirstOrDefault();
+            if (!allModels.ContainsKey(dtmi))
+            {
+                allModels.Add(dtmi, wc.DownloadString(url + $"{folder}/{root.path}"));
+            }
+            foreach (var d in root.depends)
+            {
+                GetModel(d);
+            }
+        }
+    }
+}
--- repo-tool/Program.cs
+++ repo-tool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.DigitalTwins.Parser;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,6 +16,9 @@ namespace repo_tool
         static ModelParser modelParser = new ModelParser();
         static List<string> resolverPaths = new List<string>();
         static string rootInterface = string.Empty;
+
+        static IDictionary<string, string> localFiles = new Dictionary<string, string>();
+
         static async Task Main(string[] args)
         {
             List<string> modelsContents = new List<string>();
@@ -45,9 +49,18 @@ namespace repo_tool
                 }
             }
 
+            foreach (var item in resolverPaths)
+            {
+                foreach (var f in Directory.GetFiles(item))
+                {
+                    (string id, string contents) = LightParser.SemiParse(f);
+                    localFiles.Add(id, contents);
+                }
+            }
+
             try
             {
-                modelParser.DtmiResolver = DtmiResolver;
+                modelParser.DtmiResolver = LocalDtmiResolver;
                 var parsedDtmis = await modelParser.ParseAsync(modelsContents);
                 var interfaces = parsedDtmis.Where(r => r.Value.EntityKind == DTEntityKind.Interface).ToList();
                 foreach (var dt in interfaces)
@@ -59,25 +72,30 @@ namespace repo_tool
             {
                 Console.WriteLine(ex.Message);
             }
-            
-            
         }
 
-        static private async Task<IEnumerable<string>> DtmiResolver(IReadOnlyCollection<Dtmi> dtmis)
+        static private async Task<IEnumerable<string>> LocalDtmiResolver(IReadOnlyCollection<Dtmi> dtmis)
         {
             List<String> jsonLds = new List<string>();
-            //other files in local folder as the root interface
-            //foreach (var item in resolverPaths)
-            //{
-            //    foreach (var f in Directory.GetFiles(item))
-            //    {
-            //        if (!f.Contains("model-manifest.json") && !f.Contains(rootInterface))
-            //        { 
-            //            jsonLds.Add(File.ReadAllText(f));
-            //        }
-            //    }
-            //}
+            foreach (var dtmi in dtmis)
+            {
+                if (localFiles.ContainsKey(dtmi.AbsoluteUri))
+                {
+                    jsonLds.Add(localFiles[dtmi.AbsoluteUri]);
+                }
+            }
 
+            if (jsonLds.Count<1) // not found, let's try remotely
+            {
+
+            }
+
+            return jsonLds;
+        }
+
+        static private async Task<IEnumerable<string>> RemoteDtmiResolver(IReadOnlyCollection<Dtmi> dtmis)
+        {
+            List<String> jsonLds = new List<string>();
 
             foreach (var dtmi in dtmis)
             {
@@ -109,7 +127,7 @@ namespace repo_tool
             var root = manifestItems.Where(mi => mi.dtmi == dtmi).FirstOrDefault();
             if (!allModels.ContainsKey(dtmi))
             { 
-                allModels.Add(dtmi, wc.DownloadString(url + $"{folder}/{root.path}"));
+                 allModels.Add(dtmi, wc.DownloadString(url + $"{folder}/{root.path}"));
             }
             foreach (var d in root.depends)
             {
@@ -124,5 +142,74 @@ namespace repo_tool
         public string path { get; set; }
         public string owner { get; set; }
         public IEnumerable<string> depends { get; set; } = new List<string>();
+    }
+
+    
+
+    public class RemoteResolver
+    {
+        static IDictionary<string, string> allModels = new Dictionary<string, string>();
+
+        static void GetModel(string dtmi)
+        {
+            Console.WriteLine(". . . downloading " + dtmi);
+
+            string dtmi2Folder(string dtmi)
+            {
+                var allparts = dtmi.ToLowerInvariant().Split(";")[0].Split(":");
+                var parts = allparts.Skip(1);
+                return string.Join("-", parts);
+            }
+
+            string folder = dtmi2Folder(dtmi);
+            string url = "https://ridomin.github.io/ridomodels/models/";
+
+            var wc = new WebClient();
+            var manifest = wc.DownloadString(new Uri(url + $"{folder}/model-manifest.json"));
+            var manifestItems = JsonConvert.DeserializeObject<IEnumerable<ManifestItem>>(manifest);
+            var root = manifestItems.Where(mi => mi.dtmi == dtmi).FirstOrDefault();
+            if (!allModels.ContainsKey(dtmi))
+            {
+                allModels.Add(dtmi, wc.DownloadString(url + $"{folder}/{root.path}"));
+            }
+            foreach (var d in root.depends)
+            {
+                GetModel(d);
+            }
+        }
+
+    }
+
+    public class LightParser
+    {
+        public static (string dtmi, string content) SemiParse(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException(fileName);
+            }
+            string content = File.ReadAllText(fileName);
+            JObject json;
+            try
+            {
+                json = JObject.Parse(content);
+            }
+            catch
+            {
+                throw new Newtonsoft.Json.JsonReaderException("cant parse json");
+            }
+
+
+            var ctx = json.SelectToken("@context");
+            if (ctx.Value<string>() != "dtmi:dtdl:context;2")
+            {
+                throw new ApplicationException("Not valid DTDL v2");
+            }
+
+            var id = json.SelectToken("@id");
+
+
+            return (id.Value<string>(), content);
+        }
     }
 }
